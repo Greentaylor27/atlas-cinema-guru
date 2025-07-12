bf4a306b2b80ff640ffab183bfcbd7c9fb329984
Greentaylor27 <greentaylor18@gmail.com>
2025-07-12 14:47:45 -0500

    Header is finished half way done the layout.

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { BsFilm } from "react-icons/bs";
 import { useSession } from "next-auth/react"
+import LogoutButton from "./LogoutButton";
 
 export default function Header() {
   const { data: session} = useSession();
@@ -10,12 +11,15 @@ export default function Header() {
     <nav className="flex items-center justify-between p-4 bg-[#54F4D0] text-[#00003c] shadow-md">
       <div className="flex items-center gap-2">
         <BsFilm className="text-2xl" />
-        <h1 className="text-xl font-semibold tracking-wide">Cinema Guru</h1>
+        <h1 className="text-xl font-bold tracking-wide">Cinema Guru</h1>
       </div>
 
-      {session?.user?.email && (
-        <p className="text-sm font-medium">Welcome, {session.user.email}</p>
-      )}
+      <div className="flex items-center gap-4">
+        <p className="text-sm">
+          Welcome, {session?.user?.email ? `${session.user.email}` : ''}
+        </p>
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,7 +1,15 @@
+import { FiLogOut } from "react-icons/fi";
+import { signOut } from "next-auth/react";
+
 export default function LogoutButton() {
   return (
-    <div>
-      
-    </div>
+    <button
+      onClick={() => signOut()}
+      type="button"
+      className="flex items-center gap-2 cursor-pointer"
+    >
+      <FiLogOut />
+      <span>Logout</span>
+    </button>
   )
 }
